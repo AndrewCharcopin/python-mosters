8888c6bb65b67d6dad8067a3fe1300b03daff7ad
masaki_minamide <masakiminamide@gmail.com>
2019-06-19 12:44:34 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+# if __name__ == "__main__":
+from player.py import Player
+import random
+
+##load file or initialize
+
+#king gives you 100 gold
+
+phase = 0
+# #1 Adventure
+##initialize enemy
+##Fight
+
+if(win):
+    phase+=1
+    #update lvl, status
+elif(lose):
+    record = {phase: int, playername, status:{"hp":int, "mp":int, "str": int, "vit":int}, item:{str}, money:int}
+    file.write(record)
+
+#choices: adventure, shop, lake
+#Adventure loop 
+while(True):
+    #Shop appears with 1/4 chance
+    shop = True if 1 == random.randint(4) else False
+    #Lake appears with 1/5 chance
+    lake = True if 1 == random.randint(5) else False
+    if(shop):
+        pass
+    elif(lake):
+        pass
+    else:
+        pass
+
+
--- player.py
+++ player.py
@@ -1,8 +1,8 @@
 ##initialize player if no player is selected at first
 ##load player if selected
 
-class Player(Status):
-    def __init__(self):
+class Player():
+    def __init__(self, status):
         self.status = {}
     def init_player():
         pass
