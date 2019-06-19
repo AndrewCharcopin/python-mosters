2eb3b2bbd74508130a5d99f53827e3f799d8254c
masaki_minamide <masakiminamide@gmail.com>
2019-06-19 17:05:12 +0900

    add models template and battle, main class

--- main.py
+++ main.py
@@ -1,41 +1,88 @@
-# if __name__ == "__main__":
-from player.py import Player
+from models.py import *
 import random
-file = open("record.txt")
 
-#### Initialize Player ####
-#load from file or initialize player
-#if user chooses to load, open file. Or initialize player
 
-#king gives you 100 gold.
+class Main():
+    def __init__(self):
+        # Open file if exsisted, otherwise make a file
+        self.file = open("record.txt, w")
+        # Load other necessary files e.g. sound, image
 
-#Initialize phase. Increment every time player win.
-phase = 0
+        self.init_game()
 
-# #1 Adventure
-##initialize enemy
-##Fight, lvl up
+    def init_game(self):
+        # show title
+        self.game_state = TITLE
+        #Initialize phase. Increment every time player win.
+        self.phase = 0
 
-if(win):
-    phase+=1
-    #update lvl, status
-elif(lose):
-    record = {phase: int, playername, status:{"hp":int, "mp":int, "str": int, "vit":int}, item:{str}, money:int}
-    file.write(record)
+        #### Initialize Player ####
+        #load from file or initialize player
+        #if user chooses to load, open file. Or initialize player
+        i = input("Type player number to load, or type 0 to initialize.")
+        if(i == 0):
+            self.player = Player()
+        else:
+            # check if the player exists in record, then initialize player with the value
+            self.player = Player(file[i].status))
+        
+        #### King gives you 100 gold ####
 
-##### ENTER LOOP ####
-#choices: adventure, shop, lake 
-while(True):
-    #Shop appears with 1/4 chance
-    shop = True if 1 == random.randint(4) else False
-    #Lake appears with 1/5 chance
-    lake = True if 1 == random.randint(5) else False
-    if(shop):
-        pass
-    elif(lake):
-        pass
-    #Adventure
-    else:
-        pass
+        self.game_state = ADVENTURE
 
-close(file)
+        #### Initialize enemy, Battle
+        enemy = Enemy()
+        b = Battle(player, enemy)
+        b.fight()
+        if(result == WIN):
+            b.win()
+        elif(result == LOSE):
+            b.lose()
+            
+
+        ##### ENTER LOOP ####
+        #choices: adventure, shop, lake 
+        while(True):
+            #Shop appears with 1/4 chance
+            shop = True if 1 == random.randint(4) else False
+            #Lake appears with 1/5 chance
+            lake = True if 1 == random.randint(5) else False
+            if(shop):
+                pass
+            elif(lake):
+                pass
+            #Battle
+            else:
+                b = Battle(player, enemy)
+
+
+        close(file)
+
+class Battle():
+    def __init__(self, player, enemy):
+        self.player = player
+        self.enemy = enemy
+    def fight(self):
+        result = None
+        if(self.player.status.hp > self.enemy.status.hp):
+            result = WIN
+        else:
+            result = LOST
+        return result
+
+    def win(self):
+        player.lvlup()
+        phase +=1
+
+    def lose(self):
+        record = {
+            phase: self.phase, playername: player.name, 
+            status:{"hp":player.status.hp, "mp":player.status.mp, "str": player.status.str, "vit":player.status.vit}, 
+            item:{player.items}, money:player.money
+            }
+        file.write(record)
+        
+    
+
+if __name__ == "__main__":
+    Main()
---
+++ models.py
@@ -0,0 +1,29 @@
+##initialize player if no player is selected at first
+##load player if selected
+
+class Model():
+    def __init__(self, status):
+    # if status is given, take that status
+    if(status):
+        self.status = status
+    # if is NOT given, initialize the status with random value within range
+    else:
+        self.status = {}
+    self.money = 0
+    self.items = []
+
+class Player(Model):
+    def __init__(self, status):
+        pass
+    def lvlup():
+        pass
+    def getStatus():
+        pass
+    def healHp():
+        pass
+    
+    
+class Enemy(Model):
+    def __init__(self, phase):
+        ##phaseが増えるにつれ強くする
+
