9116900834f02087ace8058d7f9b2eceacf2cd4a
masaki_minamide <masakiminamide@gmail.com>
2019-07-23 20:33:08 +0900

    add write_csv function

--- record.py
+++ record.py
@@ -1,34 +1,10 @@
-class Record():
-  
-  def __init__(self, name, stage):
-    self.time = self.get_current_time()
-    self.name = name
-    self.stage = stage
-  
-  def get_current_time():
-    now = time.ctime()
-    cnvtime = time.strptime(now)
-    return time.strftime("%Y/%m/%d %H:%M", cnvtime) 
+def write_csv(name, stage):
+  import csv, time
+  now = time.ctime()
+  cnvtime = time.strptime(now)
+  time.strftime("%Y/%m/%d %H:%M", cnvtime) 
+  with open('./record.csv','a') as f:
+    writer = csv.writer(f)
+    writer.writerow([time.strftime("%Y/%m/%d %H:%M", cnvtime) , name, stage])
 
-  def get_current_time(self):
-    import time
-    now = time.ctime()
-    cnvtime = time.strptime(now)
-    return time.strftime("%Y/%m/%d %H:%M", cnvtime)     
-
-  def write_csv(self):
-    import csv
-    import pprint
-    with open('./record.csv','a') as f:
-      writer = csv.writer(f)
-      writer.writerow([self.time, self.name, self.stage])
-  
-  #return datas in array like [[self.time, self.name, self.stage],[...]]
-  def read_csv(self):
-    import csv
-    import pprint
-    with open('./record.csv') as f:
-      reader = csv.reader(f)
-      datas = [row for row in reader]
-      return datas
-
+write_csv("aaaa", 5)
--- setting.py
+++ setting.py
@@ -38,6 +38,16 @@ def Bg(num):
 def Transform(item, width, height):
   return pygame.transform.scale(item, (width, height))
 
+def write_csv(name, stage):
+  import csv, time
+  now = time.ctime()
+  cnvtime = time.strptime(now)
+  time.strftime("%Y/%m/%d %H:%M", cnvtime) 
+  with open('./record.csv','a') as f:
+    writer = csv.writer(f)
+    writer.writerow([time.strftime("%Y/%m/%d %H:%M", cnvtime) , name, stage])
+
+
 #setting others
 clock = pygame.time.Clock()
 keys = pygame.key.get_pressed()
