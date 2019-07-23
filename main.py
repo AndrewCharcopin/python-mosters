d1056c528b3778e5cdf2c13f208c03c362e66234
masaki_minamide <masakiminamide@gmail.com>
2019-07-23 23:05:25 +0900

    add End animation

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import pygame
 from view.title import Title
 from view.stage import Stage
 from view.fight import Fight
-from view.animation import StartScreen
+from view.animation import StartScreen, Prince, EndScreen
 from controller.character import Player
 
 def main():
-  StartScreen()
   player = Player(Title())
+  Prince(player)
+  EndScreen()
+  StartScreen()
+  
   while player.stage < 5:
     Stage(player)
     Fight(player)
--- setting.py
+++ setting.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame, sys
+from pygame.locals import*
 
 pygame.init()
 
